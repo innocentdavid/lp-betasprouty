d2ceac486c5b3e6c04dd693f77b28a53f6fc216c
innocentdavid <paulinnocent04@gmail.com>
2024-04-10 20:01:15 +0100

    fix

--- src/components/ui/sparkles-bg.tsx
+++ src/components/ui/sparkles-bg.tsx
@@ -4,15 +4,14 @@ import { SparklesCore } from "./sparkles";
 export function SparklesPreview2({ children }: { children: ReactNode }) {
   return (
     <div
-      className="min-h-[40rem] relative w-full bg-[#65f5714d] _bg-gradient-to-tr from-[#65F5714d] to-[#01B9FF4d] flex flex-col items-center justify-center overflow-hidden rounded-md _text-white"
-      style={{
-        background: "rgb(247,247,247)",
-        backgroundImage:
-          "linear-gradient(45deg, rgba(247,247,247,1) 0%, rgba(54,141,253,1) 3%, rgba(234,135,124,1) 95%, rgba(240,144,109,1) 100%)",
-      }}
+      className="min-h-[40rem] relative w-full bg-black _bg-gradient-to-tr _from-[#65F5714d] _to-[#01B9FF4d] flex flex-col items-center justify-center overflow-hidden rounded-md _text-white"
+      // style={{
+      //   background: "rgb(247,247,247)",
+      //   backgroundImage:
+      //     "linear-gradient(45deg, rgba(247,247,247,1) 0%, rgba(54,141,253,1) 3%, rgba(234,135,124,1) 95%, rgba(240,144,109,1) 100%)",
+      // }}
     >
-      <div className="w-full absolute inset-0 h-screen">
-        hello
+      <div className="w-full absolute inset-0 _h-screen">
         <SparklesCore
           id="tsparticlesfullpage"
           background="transparent"
